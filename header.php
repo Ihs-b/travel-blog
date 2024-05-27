c7d14cc3a8aa55dc33cefe973d6e139f4925bcd6
sane ben <tefl.ben10@gmail.com>
2024-05-27 11:23:08 +0200

    contact page connected

---
+++ header.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta content="width=device-width, initial-scale=1.0" name="viewport">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link href="https://getbootstrap.com/docs/5.3/assets/css/docs.css" rel="stylesheet">
+    <title>Where can we take you?</title>
+    <link href="css/style.css" rel="stylesheet">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
+    <script crossorigin="anonymous" src="https://kit.fontawesome.com/061abbb655.js"></script>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Inspiration&display=swap" rel="stylesheet">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Courgette&family=Inspiration&family=Pacifico&display=swap">
+</head>
+<header class="header">
+        <h1 class="titre"><a href="index.html">Your Travel Tales</a></h1>
+        
+        <nav class="liens">
+            <hr>
+            <ul class="navbar-1">
+                <li><a href="index.html">Posts</a></li>
+                <li><a href="login.html">Se Connecter</a></li>
+                <li><a href="signin.html">S'inscrire</a></li>
+            </ul>
+            <hr>
+        </nav>
+       
+        <div class="barre">
+            <h1 class="titre_2">Contactez-nous</h1>
+        </div>
+    </header>
